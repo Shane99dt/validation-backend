44347d2b7a1fa63002af9ba51c1b5f1e95e339d7
Shane99dt <dushen1999thilaksha@gmail.com>
2022-08-25 11:03:00 +0200

    added a good method to verify city

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,6 +5,9 @@ const { checkIfExists } = require('../RoutesMW/usersMW')
 const slugify = require('slugify')
 const { body, validationResult } = require('express-validator')
 
+const passwordLessMessage = 'Password needs to be more than 8 characters'
+const passwordMoreMessage = 'Password needs to be less than 20 characters'
+
 app.get('/', (req, res) => {
   res.json(users).status(201)
 })
@@ -13,12 +16,19 @@ app.get('/:slug', checkIfExists, (req, res) => {
   res.json(req.user).status(201)
 })
 
+
+
+
 app.post(
   '/',
-  body('name').exists().isLength({min: 4}),
-  body('password').exists().isLength({min: 8}).isLength({max: 20}),
-  body('email').exists().isEmail(),
-  body('city').exists().isIn(['Paris', 'Tokyo', 'Los Angeles']),
+  body('name').exists().isLength({min: 4}).withMessage('Name is not long enough'),
+  body('password').exists().isLength({min: 8}).withMessage(passwordLessMessage).isLength({max: 20}).withMessage(passwordMoreMessage),
+  body('email').exists().isEmail().withMessage('Not an email'),
+  body('city').exists().custom(value => {
+    const slugified = slugify(value, { lower : true})
+    const slugifiedCities = ["paris", "tokyo", "los-angeles"]
+    return slugifiedCities.includes(slugified)
+  }).withMessage('City is invalid'),
   body('profile_picture').optional(),
   (req, res) => {
 
